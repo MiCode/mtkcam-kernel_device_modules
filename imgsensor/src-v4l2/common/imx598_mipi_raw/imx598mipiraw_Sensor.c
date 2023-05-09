c9a6cec7119588555cfddacd8ba47bc109a2ce87
HungWen Hsieh <hung-wen.hsieh@mediatek.com>
2023-05-09 16:16:17 +0800

    [ALPS07970948] imx598: fix power on delay time
    [Detail]
    according to ldo specification to adjust delay time of power on sequence
    
    MTK-Commit-Id: 1c947687e0ae505cac10e30c61c7ab21be8eb6a9
    
    CR-Id: ALPS07970948
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Ie53e9b038cad1c01f6baeecc3a6ad596085df908

--- imgsensor/src-v4l2/common/imx598_mipi_raw/imx598mipiraw_Sensor.c
+++ imgsensor/src-v4l2/common/imx598_mipi_raw/imx598mipiraw_Sensor.c
@@ -686,16 +686,16 @@ static struct subdrv_ops ops = {
 
 static struct subdrv_pw_seq_entry pw_seq[] = {
 	{HW_ID_MCLK, 24, 0},
-	{HW_ID_RST, 0, 1},
-	{HW_ID_AVDD2, 1800000, 1}, // pmic_gpo(2.8V ldo) for avdd
-	{HW_ID_AVDD, 2800000, 1}, // pmic_ldo for avdd
-	{HW_ID_AVDD4, 1800000, 1}, // pmic_ldo/pmic_gpo(1.8V ldo) for avdd1
-	{HW_ID_AFVDD1, 1800000, 1}, // pmic_gpo(2.8V ldo) for afvdd
-	{HW_ID_AFVDD, 2800000, 1}, // pmic_ldo for afvdd
-	{HW_ID_DOVDD, 1800000, 1}, // pmic_ldo/gpio(1.8V ldo) for dovdd
-	{HW_ID_DVDD, 1100000, 1}, // pmic_ldo for dvdd
+	{HW_ID_RST, 0, 0},
 	{HW_ID_MCLK_DRIVING_CURRENT, 4, 1},
-	{HW_ID_RST, 1, 5}
+	{HW_ID_AVDD2, 1800000, 0}, // pmic_gpo(2.8V ldo) for avdd
+	{HW_ID_AVDD, 2800000, 0}, // pmic_ldo for avdd
+	{HW_ID_AVDD4, 1800000, 0}, // pmic_ldo/pmic_gpo(1.8V ldo) for avdd1
+	{HW_ID_AFVDD1, 1800000, 0}, // pmic_gpo(2.8V ldo) for afvdd
+	{HW_ID_AFVDD, 2800000, 0}, // pmic_ldo for afvdd
+	{HW_ID_DOVDD, 1800000, 0}, // pmic_ldo/gpio(1.8V ldo) for dovdd
+	{HW_ID_DVDD, 1100000, 1}, // pmic_ldo for dvdd
+	{HW_ID_RST, 1, 1}
 };
 
 const struct subdrv_entry imx598_mipi_raw_entry = {

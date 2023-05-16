89859bfb3665926b3e93fb12b557f9eb900b3e46
mtk23161 <yuche.huang@mediatek.com>
2023-05-16 09:32:54 +0800

    [ALPS07922396] refine adaptor trace api
    1. remove trace printk
    
    MTK-Commit-Id: 39815e5fe53d7d5cea2575a0009c6a15b9c53e87
    
    CR-Id: ALPS07922396
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I1d07b83c545a4eb5c1111e0ed71f0b26f040d438
    Signed-off-by: mtk23161 <yuche.huang@mediatek.com>

--- imgsensor/src-v4l2/adaptor-trace.h
+++ imgsensor/src-v4l2/adaptor-trace.h
@@ -4,26 +4,48 @@
  *
  */
 
-#ifndef _ADAPTOR_TRACE_H__
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM adaptor
+
+#if !defined(_ADAPTOR_TRACE_H__) || defined(TRACE_HEADER_MULTI_READ)
 #define _ADAPTOR_TRACE_H__
+
+#include <linux/tracepoint.h>
+#include <linux/trace_events.h>
+
+TRACE_EVENT(tracing_mark_write,
+	TP_PROTO(const char *fmt, va_list *va),
+	TP_ARGS(fmt, va),
+	TP_STRUCT__entry(
+		__vstring(vstr, fmt, va)
+	),
+	TP_fast_assign(
+		__assign_vstr(vstr, fmt, va);
+	),
+	TP_printk("%s", __get_str(vstr))
+);
+
+#undef TRACE_INCLUDE_PATH
+#undef TRACE_INCLUDE_FILE
+#define TRACE_INCLUDE_PATH .
+#define TRACE_INCLUDE_FILE adaptor-trace
+/* This part must be outside protection */
+
+#include <trace/define_trace.h>
 
 #include <linux/kernel.h>
 #include <linux/sched.h>
-#include <linux/trace_events.h>
-#define ADAPTOR_FTRACE
-#ifdef ADAPTOR_FTRACE
 
-#define BUF_LENGTH 256
+#define ADAPTOR_TRACE_FORCE_BEGIN(catetory, fmt, args...)	\
+	__adaptor_systrace("B|%d|%s" fmt, task_tgid_nr(current), catetory, ##args)
 
-#define ADAPTOR_TRACE_FORCE_BEGIN(fmt, args...)	\
-	__adaptor_systrace_b(fmt, ##args)
-
-#define ADAPTOR_TRACE_FORCE_END()	__adaptor_systrace_e()
+#define ADAPTOR_TRACE_FORCE_END()	\
+	__adaptor_systrace("E|%d", task_tgid_nr(current))
 
 
 #define ADAPTOR_SYSTRACE_BEGIN(fmt, args...) do { \
 	if (adaptor_trace_enabled()) { \
-		ADAPTOR_TRACE_FORCE_BEGIN(fmt, ##args); \
+		ADAPTOR_TRACE_FORCE_BEGIN("adaptor::", fmt, ##args); \
 	} \
 } while (0)
 
@@ -33,15 +55,8 @@
 	} \
 } while (0)
 
-bool adaptor_trace_enabled(void);
-void __adaptor_systrace_b(const char *fmt, ...);
-void __adaptor_systrace_e(void);
+bool inline adaptor_trace_enabled(void);
+void __adaptor_systrace(const char *fmt, ...);
 
-#else
 
-#define ADAPTOR_SYSTRACE_BEGIN(fmt, args...)
-#define ADAPTOR_SYSTRACE_END()
-
-#endif
-
-#endif
+#endif  /* _ADAPTOR_TRACE_H__ */

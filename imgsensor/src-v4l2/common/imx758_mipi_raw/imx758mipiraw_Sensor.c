28658fb01609fba214d804d296c93cb79ef2593b
Jeff Teng <jeff.teng@mediatek.com>
2023-05-31 15:31:43 +0800

    [ALPS07939675] imx758 powewr on seq reduce delay time
    1.8ms -> 3ms
    
    MTK-Commit-Id: a8990feea4b27077951ef150b7da9e259288e6cf
    
    Signed-off-by: Jeff Teng <jeff.teng@mediatek.com>
    CR-Id: ALPS07939675
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Iecf13c90e94bf4ed3f3e1eb291a536d76a5136e5

--- imgsensor/src-v4l2/common/imx758_mipi_raw/imx758mipiraw_Sensor.c
+++ imgsensor/src-v4l2/common/imx758_mipi_raw/imx758mipiraw_Sensor.c
@@ -776,15 +776,15 @@ static struct subdrv_ops ops = {
 
 static struct subdrv_pw_seq_entry pw_seq[] = {
 	{HW_ID_MCLK, 24, 0},
-	{HW_ID_RST, 0, 1},
-	{HW_ID_DOVDD, 1800000, 1}, // pmic_ldo/gpio(1.8V ldo) for dovdd
+	{HW_ID_RST, 0, 0},
+	{HW_ID_MCLK_DRIVING_CURRENT, 4, 1},
 	{HW_ID_AVDD, 2800000, 0}, // pmic_ldo for avdd
 	{HW_ID_AFVDD, 2800000, 0}, // pmic_ldo for afvdd
-	{HW_ID_DVDD, 1100000, 0}, // pmic_ldo/gpio(1.1V ldo) for dvdd
+	{HW_ID_DOVDD, 1800000, 0}, // pmic_ldo/gpio(1.8V ldo) for dovdd
 	{HW_ID_OISVDD, 2800000, 0}, // pmic_ldo for oisvdd
 	{HW_ID_OISEN, 2800000, 0}, // gpio for oisen
-	{HW_ID_MCLK_DRIVING_CURRENT, 4, 1},
-	{HW_ID_RST, 1, 5}
+	{HW_ID_DVDD, 1100000, 1}, // pmic_ldo/gpio(1.1V ldo) for dvdd
+	{HW_ID_RST, 1, 1}
 };
 
 const struct subdrv_entry imx758_mipi_raw_entry = {

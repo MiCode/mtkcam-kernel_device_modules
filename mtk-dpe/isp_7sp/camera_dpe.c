ec1971cf225313c30a8adfb1ee58450e2db36245
Bryan-CC Huang <Bryan-CC.Huang@mediatek.com>
2023-06-27 09:06:17 +0800

    [ALPS08161453] DPE: fix syzkaller error
    [Detail]
    
    fix syzkaller error
    
    MTK-Commit-Id: 13f93a5c0410d8ad32243a8aa3ebe2b8a93922ca
    
    Signed-off-by: Bryan-CC Huang <Bryan-CC.Huang@mediatek.com>
    CR-Id: ALPS08161453
    Feature: Vulnerability Scan
    Change-Id: Iea2a7a95cf49d1779e5038bfed06e177d38d4fb3

--- mtk-dpe/isp_7sp/camera_dpe.c
+++ mtk-dpe/isp_7sp/camera_dpe.c
@@ -6512,6 +6512,7 @@ static inline void DPE_Reset(void)
 /*******************************************************************************
  *
  ******************************************************************************/
+#ifdef DPE_ioctl_en
 static signed int DPE_ReadReg(struct DPE_REG_IO_STRUCT *pRegIo)
 {
 	unsigned int i;
@@ -6560,9 +6561,11 @@ static signed int DPE_ReadReg(struct DPE_REG_IO_STRUCT *pRegIo)
 EXIT:
 	return Ret;
 }
+#endif
 /*******************************************************************************
  *
  ******************************************************************************/
+#ifdef DPE_ioctl_en
 static signed int DPE_WriteRegToHw(struct DPE_REG_STRUCT *pReg,
 							unsigned int Count)
 {
@@ -6597,9 +6600,11 @@ static signed int DPE_WriteRegToHw(struct DPE_REG_STRUCT *pReg,
 	/*  */
 	return Ret;
 }
+#endif
 /*******************************************************************************
  *
  ******************************************************************************/
+#ifdef DPE_ioctl_en
 static signed int DPE_WriteReg(struct DPE_REG_IO_STRUCT *pRegIo)
 {
 	signed int Ret = 0;
@@ -6651,9 +6656,11 @@ EXIT:
 	}
 	return Ret;
 }
+#endif
 /*******************************************************************************
  *
  ******************************************************************************/
+#ifdef DPE_ioctl_en
 static signed int DPE_WaitIrq(struct DPE_WAIT_IRQ_STRUCT *WaitIrq)
 {
 	signed int Ret = 0;
@@ -6804,6 +6811,7 @@ static signed int DPE_WaitIrq(struct DPE_WAIT_IRQ_STRUCT *WaitIrq)
 EXIT:
 	return Ret;
 }
+#endif
 /*******************************************************************************
  *
  ******************************************************************************/
@@ -6811,9 +6819,11 @@ static long DPE_ioctl(struct file *pFile, unsigned int Cmd, unsigned long Param)
 {
 	signed int Ret = 0;
 	/*unsigned int pid = 0;*/
+	#ifdef DPE_ioctl_en
 	static struct DPE_REG_IO_STRUCT RegIo;
 	static struct DPE_WAIT_IRQ_STRUCT IrqInfo;
 	static struct DPE_CLEAR_IRQ_STRUCT ClearIrq;
+	#endif
 	static struct DPE_Config_V2 dpe_DpeConfig;
 	static struct DPE_Request dpe_DpeReq;
 	// signed int enqnum;
@@ -6870,6 +6880,8 @@ static long DPE_ioctl(struct file *pFile, unsigned int Cmd, unsigned long Param)
 		}
 	case DPE_READ_REGISTER:
 		{
+			LOG_INF("Not support DPE ioctl DPE_READ_REGISTER\n");
+			#ifdef DPE_ioctl_en
 			if (copy_from_user(&RegIo, (void *)Param,
 				sizeof(struct DPE_REG_IO_STRUCT)) == 0) {
 				Ret = DPE_ReadReg(&RegIo);
@@ -6878,10 +6890,13 @@ static long DPE_ioctl(struct file *pFile, unsigned int Cmd, unsigned long Param)
 				"DPE_READ_REGISTER copy_from_user failed");
 				Ret = -EFAULT;
 			}
+			#endif
 			break;
 		}
 	case DPE_WRITE_REGISTER:
 		{
+			LOG_INF("Not support DPE ioctl DPE_WRITE_REGISTER\n");
+			#ifdef DPE_ioctl_en
 			if (copy_from_user(&RegIo, (void *)Param,
 				sizeof(struct DPE_REG_IO_STRUCT)) == 0) {
 				Ret = DPE_WriteReg(&RegIo);
@@ -6890,13 +6905,16 @@ static long DPE_ioctl(struct file *pFile, unsigned int Cmd, unsigned long Param)
 				"DPE_WRITE_REGISTER copy_from_user failed");
 				Ret = -EFAULT;
 			}
+			#endif
 			break;
 		}
 	case DPE_WAIT_IRQ:
 		{
+			LOG_INF("Not support DPE ioctl DPE_WAIT_IRQ\n");	
+			#ifdef DPE_ioctl_en
 			if (copy_from_user(&IrqInfo, (void *)Param,
 				sizeof(struct DPE_WAIT_IRQ_STRUCT)) == 0) {
-				/*  */
+
 				if ((IrqInfo.Type >= DPE_IRQ_TYPE_AMOUNT) ||
 							(IrqInfo.Type < 0)) {
 					Ret = -EFAULT;
@@ -6928,10 +6946,13 @@ static long DPE_ioctl(struct file *pFile, unsigned int Cmd, unsigned long Param)
 				LOG_ERR("DPE_WAIT_IRQ copy_from_user failed");
 				Ret = -EFAULT;
 			}
+			#endif
 			break;
 		}
 	case DPE_CLEAR_IRQ:
 		{
+			LOG_INF("Not support DPE ioctl DPE_CLEAR_IRQ\n");
+			#ifdef DPE_ioctl_en
 			if (copy_from_user(&ClearIrq, (void *)Param,
 				sizeof(struct DPE_CLEAR_IRQ_STRUCT)) == 0) {
 				LOG_INF("DPE_CLEAR_IRQ Type(%d)",
@@ -6943,7 +6964,7 @@ static long DPE_ioctl(struct file *pFile, unsigned int Cmd, unsigned long Param)
 								ClearIrq.Type);
 					goto EXIT;
 				}
-				/*  */
+
 				if ((ClearIrq.UserKey >= IRQ_USER_NUM_MAX)
 				    || (ClearIrq.UserKey < 0)) {
 					LOG_ERR("errUserEnum(%d)",
@@ -6966,6 +6987,7 @@ static long DPE_ioctl(struct file *pFile, unsigned int Cmd, unsigned long Param)
 				"DPE_CLEAR_IRQ copy_from_user failed\n");
 				Ret = -EFAULT;
 			}
+			#endif
 			break;
 		}
 	case DPE_ENQNUE_NUM:

2755c5498b25679a9a34c9c8dffa27458e6da13c
Clive Lin <clive.lin@mediatek.com>
2024-01-12 20:54:13 +0800

    [ALPS08490829] remove the use of dma_buf lock
    commit 28743e25fa1c ("dma-buf: Remove obsoleted internal lock")
    removes the internal lock from dma_buf, so follow it to remove
    the use of lock lock to fix the following errors:
    
    vendor/mediatek/kernel_modules/mtkcam/mtk-aie/mem/aie_videobuf2-dma-contig.c:285:43: error: no member named lock in struct dma_buf
            struct mutex *lock = &db_attach->dmabuf->lock;
                                  ~~~~~~~~~~~~~~~~~  ^
    vendor/mediatek/kernel_modules/mtkcam/mtk-aie/mem/aie_videobuf2-vmalloc.c:265:43: error: no member named lock in struct dma_buf
            struct mutex *lock = &db_attach->dmabuf->lock;
                                  ~~~~~~~~~~~~~~~~~  ^
    vendor/mediatek/kernel_modules/mtkcam/mtk-dpe/isp_7sp/videobuf2-dma-contig.c:289:43: error: no member named lock in struct dma_buf
            struct mutex *lock = &db_attach->dmabuf->lock;
                                  ~~~~~~~~~~~~~~~~~  ^
    vendor/mediatek/kernel_modules/mtkcam/mtk-dpe/isp_7s/videobuf2-dma-contig.c:289:43: error: no member named lock in struct dma_buf
            struct mutex *lock = &db_attach->dmabuf->lock;
                                  ~~~~~~~~~~~~~~~~~  ^
    
    MTK-Commit-Id: bfb75c3e27199f5e98a41baa8460aaae7b333e68
    
    Signed-off-by: Clive Lin <clive.lin@mediatek.com>
    CR-Id: ALPS08490829
    Feature: Kernel SI Operation
    Change-Id: If5eb780e49e8135824832e89888f1c5244f00d42

--- mtk-dpe/isp_7sp/videobuf2-dma-contig.c
+++ mtk-dpe/isp_7sp/videobuf2-dma-contig.c
@@ -285,18 +285,12 @@ static struct sg_table *vb2_dc_dmabuf_ops_map(
 	struct dma_buf_attachment *db_attach, enum dma_data_direction dma_dir)
 {
 	struct vb2_dc_attachment *attach = db_attach->priv;
-	/* stealing dmabuf mutex to serialize map/unmap operations */
-	struct mutex *lock = &db_attach->dmabuf->lock;
 	struct sg_table *sgt;
-
-	mutex_lock(lock);
 
 	sgt = &attach->sgt;
 	/* return previously mapped sg table */
-	if (attach->dma_dir == dma_dir) {
-		mutex_unlock(lock);
+	if (attach->dma_dir == dma_dir)
 		return sgt;
-	}
 
 	/* release any previous cache */
 	if (attach->dma_dir != DMA_NONE) {
@@ -310,13 +304,10 @@ static struct sg_table *vb2_dc_dmabuf_ops_map(
 				dma_dir);
 	if (!sgt->nents) {
 		pr_info("failed to map scatterlist\n");
-		mutex_unlock(lock);
 		return ERR_PTR(-EIO);
 	}
 
 	attach->dma_dir = dma_dir;
-
-	mutex_unlock(lock);
 
 	return sgt;
 }

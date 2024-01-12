10681d6aae0b9b097d97eb330b03d34506a8111f
mtk03483 <marvin.lin@mediatek.com>
2024-01-12 20:53:01 +0800

    [ALPS08274077] imgsys: fix deadlock issue
    [Detail]
    fix mutex deadlock issue
    
    MTK-Commit-Id: 18eda5c1df72022dd2be35d451ee66f6d3bc7966
    
    Signed-off-by: mtk03483 <marvin.lin@mediatek.com>
    CR-Id: ALPS08274077
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I5deb5436b936f93643a51f109c2f6eefa3f3be83

--- mtk-hcp/mtk-hcp_kernelfence.c
+++ mtk-hcp/mtk-hcp_kernelfence.c
@@ -98,6 +98,7 @@ static int mtk_hcp_acquire_KernelFence(unsigned int *fds, int fd_num)
 		sync_file = sync_file_create(&f_entry->kFence);
 		if (sync_file == NULL) {
 			pr_info("sync_file create fail\n");
+            mutex_unlock(&fence_info_list->fence_lock);
 			return -EINVAL;
 		}
 		fds[i] = get_unused_fd_flags(O_CLOEXEC);
@@ -143,13 +144,13 @@ static int mtk_hcp_release_KernelFence(unsigned int *fds, int fd_num)
 				pr_info("set fence release imgsys_fw-(%d/%d/%p)\n",
 						i, fds[i], &f_entry->kFence);
 				f_entry->release_fence = 1;
-				mutex_unlock(&fence_info_list->fence_lock);
 				break;
 			}
 		}
         if (hcp_dbg_enable())
 		pr_debug("imgsys_fw-(%d/%d)--\n", i, fds[i]);
 	}
+    mutex_unlock(&fence_info_list->fence_lock);
 	return 0;
 }
 

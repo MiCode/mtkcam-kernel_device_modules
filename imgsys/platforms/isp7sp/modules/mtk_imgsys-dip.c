de404e6258b06b920887f6b24791b8e2fd41057c
mtk03483 <marvin.lin@mediatek.com>
2024-01-12 20:30:03 +0800

    [ALPS07942565] imgsys/dpe: sync code
    [Detail]
    sync code to camera repo
    
    MTK-Commit-Id: ffde43d743f9c9a6b00edd57cd909ebe5c7cbbca
    
    Signed-off-by: mtk03483 <marvin.lin@mediatek.com>
    CR-Id: ALPS07942565
    Feature: IMGSYS_IPESYS
    Change-Id: Ib911a2046a90387ef769966abc334a93461e9cfe

--- imgsys/platforms/isp7sp/modules/mtk_imgsys-dip.c
+++ imgsys/platforms/isp7sp/modules/mtk_imgsys-dip.c
@@ -643,6 +643,27 @@ static void imgsys_dip_dump_smtd4(struct mtk_imgsys_dev *a_pDev,
 
 }
 
+static void imgsys_dip_dump_yufdd1(struct mtk_imgsys_dev *a_pDev,
+				void __iomem *a_pRegBA,
+				unsigned int a_DdbSel,
+				unsigned int a_DbgOut)
+{
+	unsigned int DbgCmd = 0;
+	unsigned int DbgData = 0;
+	unsigned int Idx = 0;
+	unsigned int CmdOft = 0x10000;
+
+	pr_info("dump yufd_d1 debug\n");
+
+	/* yufd_d1 debug */
+	DbgCmd = 0x10401;
+	for (Idx = 0; Idx < 0xF; Idx++) {
+		DbgData = ExeDbgCmd(a_pDev, a_pRegBA, a_DdbSel, a_DbgOut, DbgCmd);
+		DbgCmd += CmdOft;
+	}
+
+}
+
 void imgsys_dip_debug_dump(struct mtk_imgsys_dev *imgsys_dev,
 							unsigned int engine)
 {
@@ -840,6 +861,15 @@ void imgsys_dip_debug_dump(struct mtk_imgsys_dev *imgsys_dev,
 			(unsigned int)ioread32((void *)(dipRegBA + i + 0x8)),
 			(unsigned int)ioread32((void *)(dipRegBA + i + 0xc)));
 	}
+	/* TCY_D2 */
+	for (i = TCY_D2_CTL_OFT; i <= (TCY_D2_CTL_OFT + TCY_D2_CTL_SZ); i += 0x10) {
+		pr_info("[0x%08X] 0x%08X 0x%08X 0x%08X 0x%08X",
+			(unsigned int)(DIP_NR2_ADDR + i),
+			(unsigned int)ioread32((void *)(dipRegBA + i)),
+			(unsigned int)ioread32((void *)(dipRegBA + i + 0x4)),
+			(unsigned int)ioread32((void *)(dipRegBA + i + 0x8)),
+			(unsigned int)ioread32((void *)(dipRegBA + i + 0xc)));
+	}
 
 	/* DMA_TOP debug data */
 	DMANrPort = 0;
@@ -874,6 +904,8 @@ void imgsys_dip_debug_dump(struct mtk_imgsys_dev *imgsys_dev,
 	imgsys_dip_dump_pcrpd16(imgsys_dev, dipRegBA, CtlDdbSel, CtlDbgOut);
 	/* SMT_D4 debug data */
 	imgsys_dip_dump_smtd4(imgsys_dev, dipRegBA, CtlDdbSel, CtlDbgOut);
+	/* YUFD_D1 debug data */
+	imgsys_dip_dump_yufdd1(imgsys_dev, dipRegBA, CtlDdbSel, CtlDbgOut);
 
 	pr_info("%s: -\n", __func__);
 

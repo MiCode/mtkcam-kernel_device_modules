2666a76e86f1d03f35183422b0ea99917264324b
HungWen Hsieh <hung-wen.hsieh@mediatek.com>
2024-01-12 20:37:52 +0800

    [ALPS07851750] imx766: fix power on delay time
    [Detail]
    according to ldo specification to adjust delay time of power on sequence
    
    MTK-Commit-Id: 93cf24e78b11a3434b69cdafc96c4f9ca5bbdb63
    
    CR-Id: ALPS07851750
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Icf2d51cb7395dd7be1debd611d97aa156bae2baa

--- imgsensor/src-v4l2/common/imx766dual_mipi_raw/imx766dualmipiraw_Sensor.c
+++ imgsensor/src-v4l2/common/imx766dual_mipi_raw/imx766dualmipiraw_Sensor.c
@@ -2317,15 +2317,15 @@ static struct subdrv_ops ops = {
 static struct subdrv_pw_seq_entry pw_seq[] = {
 	{HW_ID_MCLK, 24, 0},
 	{HW_ID_PDN, 0, 0},
-	{HW_ID_RST, 0, 1},
-	{HW_ID_AVDD, 2800000, 3},
-	{HW_ID_AVDD1, 1800000, 3},
-	{HW_ID_AFVDD, 2800000, 3},
-	{HW_ID_DVDD, 1090000, 4},
+	{HW_ID_RST, 0, 0},
+	{HW_ID_MCLK_DRIVING_CURRENT, 4, 1},
+	{HW_ID_AVDD, 2800000, 0},
+	{HW_ID_AVDD1, 1800000, 0},
+	{HW_ID_AFVDD, 2800000, 0},
+	{HW_ID_DVDD, 1090000, 0},
 	{HW_ID_DOVDD, 1800000, 1},
-	{HW_ID_MCLK_DRIVING_CURRENT, 4, 6},
 	{HW_ID_PDN, 1, 0},
-	{HW_ID_RST, 1, 5}
+	{HW_ID_RST, 1, 2}
 };
 
 const struct subdrv_entry imx766dual_mipi_raw_entry = {

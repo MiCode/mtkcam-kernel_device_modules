dcb8c021d614f7ba69479e489d9394c9ef99f3c2
Tony Shen <tung.shen@mediatek.com>
2024-01-12 20:30:04 +0800

    [ALPS07851750] cam: power on IOVDD before DVDD
    [Description]
    cam0: IOVDD input power for PMIC(RT5133 LDO8) DVDD.
    cam2: IOVDD input power for PMIC(RT5133 LDO7) DVDD.
    
    MTK-Commit-Id: e7106b6fb58d47c3957fe7066dbfe51484942c19
    
    CR-Id: ALPS07851750
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Tony Shen <tung.shen@mediatek.com>
    Change-Id: I536a5d0ec0b1b5ed69c57597b6842024b5aa3982

--- imgsensor/src-v4l2/common/imx709_mipi_raw/imx709mipiraw_Sensor.c
+++ imgsensor/src-v4l2/common/imx709_mipi_raw/imx709mipiraw_Sensor.c
@@ -1303,8 +1303,8 @@ static struct subdrv_pw_seq_entry pw_seq[] = {
 	{HW_ID_RST1, 0, 1},
 	{HW_ID_AVDD, 2900000, 1}, // pmic_ldo for avdd
 	{HW_ID_AVDD2, 1800000, 1}, // pmic_gpo(2.8V ldo) for avdd
-	{HW_ID_DVDD1, 855000, 1}, // pmic_ldo for dvdd
 	{HW_ID_DOVDD, 1800000, 1}, // pmic_ldo/gpio(1.8V ldo) for dovdd
+	{HW_ID_DVDD1, 855000, 1}, // pmic_ldo for dvdd
 	{HW_ID_MCLK1_DRIVING_CURRENT, 6, 1},
 	{HW_ID_PONV, 1, 1},
 	{HW_ID_RST1, 1, 4}

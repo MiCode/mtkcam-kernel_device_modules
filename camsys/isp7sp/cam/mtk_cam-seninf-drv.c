9d53a25ec7f41d5bae0dcc798023643e329323d0
mtk23161 <yuche.huang@mediatek.com>
2024-01-12 20:39:33 +0800

    [ALPS07922396] add error handling for tag_order
    1. add error handling for get_tag_order function
    2. add log for seninf debug
    
    MTK-Commit-Id: 602c6d8378c0f61f3497107012eafc0e77ee9739
    
    CR-Id: ALPS07922396
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I8d5e38722246ab655cf996a99c0e0f2a90cd522f
    Signed-off-by: mtk23161 <yuche.huang@mediatek.com>

--- camsys/isp7sp/cam/mtk_cam-seninf-drv.c
+++ camsys/isp7sp/cam/mtk_cam-seninf-drv.c
@@ -1664,6 +1664,11 @@ static int get_buffered_pixel_rate(struct seninf_ctx *ctx,
 	struct seninf_core *core = ctx->core;
 #endif
 
+	if (sd == NULL) {
+		dev_info(ctx->dev, "[%s][error] sd is NULL, skip function\n", __func__);
+		return -EINVAL;
+	}
+
 	fmt.pad = sd_pad_idx;
 	fmt.which = V4L2_SUBDEV_FORMAT_ACTIVE;
 	ret = v4l2_subdev_call(sd, pad, get_fmt, NULL, &fmt);
@@ -1729,6 +1734,11 @@ static int get_customized_pixel_rate(struct seninf_ctx *ctx, struct v4l2_subdev 
 	struct seninf_core *core = ctx->core;
 #endif
 
+	if (sd == NULL) {
+		dev_info(ctx->dev, "[%s][error] sd is NULL, skip function\n", __func__);
+		return -EINVAL;
+	}
+
 	ctrl = v4l2_ctrl_find(sd->ctrl_handler, V4L2_CID_MTK_CUST_SENSOR_PIXEL_RATE);
 	if (!ctrl) {
 		dev_info(ctx->dev, "no cust pixel rate in subdev %s\n", sd->name);
@@ -1754,6 +1764,11 @@ static int get_pixel_rate(struct seninf_ctx *ctx, struct v4l2_subdev *sd,
 #if AOV_GET_PARAM
 	struct seninf_core *core = ctx->core;
 #endif
+
+	if (sd == NULL) {
+		dev_info(ctx->dev, "[%s][error] sd is NULL, skip function\n", __func__);
+		return -EINVAL;
+	}
 
 	ctrl = v4l2_ctrl_find(sd->ctrl_handler, V4L2_CID_PIXEL_RATE);
 	if (!ctrl) {
@@ -3745,13 +3760,16 @@ int mtk_cam_seninf_check_timeout(struct v4l2_subdev *sd, u64 time_after_sof)
 	struct v4l2_subdev *sensor_sd = ctx->sensor_sd;
 	struct v4l2_ctrl *ctrl;
 
-	ctrl = v4l2_ctrl_find(sensor_sd->ctrl_handler, V4L2_CID_MTK_SOF_TIMEOUT_VALUE);
-	if (!ctrl) {
-		dev_info(ctx->dev, "no timeout value in subdev %s\n", sd->name);
-		return -EINVAL;
-	}
+	if (sensor_sd != NULL) {  /* prevent test module ke due to sensor_sd is NULL*/
 
-	val = v4l2_ctrl_g_ctrl(ctrl);
+		ctrl = v4l2_ctrl_find(sensor_sd->ctrl_handler, V4L2_CID_MTK_SOF_TIMEOUT_VALUE);
+		if (!ctrl) {
+			dev_info(ctx->dev, "no timeout value in subdev %s\n", sd->name);
+			return -EINVAL;
+		}
+
+		val = v4l2_ctrl_g_ctrl(ctrl);
+	}
 
 	if (val > 0)
 		frame_time = val;
@@ -3777,6 +3795,12 @@ u64 mtk_cam_seninf_get_frame_time(struct v4l2_subdev *sd, u32 seq_id)
 	struct v4l2_subdev *sensor_sd = ctx->sensor_sd;
 	struct v4l2_ctrl *ctrl;
 	int val = 0;
+
+	if (sensor_sd == NULL) {
+		dev_info(ctx->dev, "[%s][error] sensor_sd is NULL, skip function and default return %llu\n",
+			__func__, tmp);
+		return tmp;
+	}
 
 	ctrl = v4l2_ctrl_find(sensor_sd->ctrl_handler, V4L2_CID_MTK_SOF_TIMEOUT_VALUE);
 	if (ctrl) {
@@ -3806,12 +3830,15 @@ int mtk_cam_seninf_dump(struct v4l2_subdev *sd, u32 seq_id, bool force_check)
 	ctx->dbg_last_dump_req = seq_id;
 	ctx->dbg_timeout = 0; //in us
 
-	ctrl = v4l2_ctrl_find(sensor_sd->ctrl_handler, V4L2_CID_MTK_SOF_TIMEOUT_VALUE);
-	if (ctrl) {
-		val = v4l2_ctrl_g_ctrl(ctrl);
-		if (val > 0)
-			ctx->dbg_timeout = val;
+	if (sensor_sd != NULL) { /* avoid sensor_sd is NULL when using sening test pattern*/
+		ctrl = v4l2_ctrl_find(sensor_sd->ctrl_handler, V4L2_CID_MTK_SOF_TIMEOUT_VALUE);
+		if (ctrl) {
+			val = v4l2_ctrl_g_ctrl(ctrl);
+			if (val > 0)
+				ctx->dbg_timeout = val;
+		}
 	}
+
 	ret = pm_runtime_get_sync(ctx->dev);
 	if (ret < 0) {
 		dev_info(ctx->dev, "%s pm_runtime_get_sync ret %d\n", __func__, ret);
@@ -3820,8 +3847,8 @@ int mtk_cam_seninf_dump(struct v4l2_subdev *sd, u32 seq_id, bool force_check)
 	}
 
 	/* query if sensor in reset */
-	sensor_sd->ops->core->command(sensor_sd,
-			V4L2_CMD_SENSOR_IN_RESET, &in_reset);
+	if (sensor_sd != NULL) /* avoid sensor_sd is NULL when using sening test pattern*/
+		sensor_sd->ops->core->command(sensor_sd, V4L2_CMD_SENSOR_IN_RESET, &in_reset);
 
 	if (ctx->streaming) {
 		if (!in_reset) {

996c1de357f5b540448c5985532f9a85113b233f
Jeff Teng <jeff.teng@mediatek.com>
2024-01-12 20:42:22 +0800

    [ALPS08149433] im758 refine ois power seq
    im758 refine ois power seq
    
    MTK-Commit-Id: b8a03e39b3dec185ae07e32cdddf659b329683e2
    
    Signed-off-by: Jeff Teng <jeff.teng@mediatek.com>
    CR-Id: ALPS08149433
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Ife1d67e1328d986282636154d848c8f3f07a11c6

--- imgsensor/src-v4l2/common/imx758_mipi_raw/imx758mipiraw_Sensor.c
+++ imgsensor/src-v4l2/common/imx758_mipi_raw/imx758mipiraw_Sensor.c
@@ -780,11 +780,11 @@ static struct subdrv_pw_seq_entry pw_seq[] = {
 	{HW_ID_MCLK, 24, 0},
 	{HW_ID_RST, 0, 0},
 	{HW_ID_MCLK_DRIVING_CURRENT, 4, 1},
+	{HW_ID_OISVDD, 2800000, 0}, // pmic_ldo for oisvdd
+	{HW_ID_OISEN, 2800000, 1}, // gpio for oisen
 	{HW_ID_AVDD, 2800000, 0}, // pmic_ldo for avdd
 	{HW_ID_AFVDD, 2800000, 0}, // pmic_ldo for afvdd
 	{HW_ID_DOVDD, 1800000, 0}, // pmic_ldo/gpio(1.8V ldo) for dovdd
-	{HW_ID_OISVDD, 2800000, 0}, // pmic_ldo for oisvdd
-	{HW_ID_OISEN, 2800000, 0}, // gpio for oisen
 	{HW_ID_DVDD, 1100000, 1}, // pmic_ldo/gpio(1.1V ldo) for dvdd
 	{HW_ID_RST, 1, 1}
 };

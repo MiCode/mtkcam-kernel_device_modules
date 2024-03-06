d2db5cd9f42bf630b8ae05ec0468077bb8350337
Roy Yang <roy.yang@mediatek.com>
2024-03-06 10:11:59 +0800

    [ALPS08751695] camsys: isp8: refine cq validation
    [Description]
    refine the deadline for cq exe
    
    [Test]
    Build pass and self-sanity pass
    
    MTK-Commit-Id: e329772a72fd1b2c874f0f9141f31265e2a9e164
    
    CR-Id: ALPS08751695
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I3912e8c35bae259e8d29489e9f0cdd8427a107cd
    Signed-off-by: Roy Yang <roy.yang@mediatek.com>

--- camsys/isp8/cam/mtk_cam-job_state_impl_guard.h
+++ camsys/isp8/cam/mtk_cam-job_state_impl_guard.h
@@ -210,7 +210,7 @@ static inline bool valid_cq_execution_avoid_race_with_topirq(
 	if (unlikely(!p->s_params))
 		return ret;
 
-	ret = (p->event_ts - p->info->sof_ts_ns) > 300000 ? true : false;
+	ret = (p->event_ts - p->info->sof_l_ts_ns) > 30000000 ? false : true;
 
 	if (ret == false)
 		pr_info("[DEBUG] race with top-half case, event/f_sof/l_sof:%llu/%llu/%llu (%llu)",

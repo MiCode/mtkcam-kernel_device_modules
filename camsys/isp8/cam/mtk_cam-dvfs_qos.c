c5615998d13d98d69090f87c7b693aaa9fd3acf0
Roy Yang <roy.yang@mediatek.com>
2024-03-19 09:56:34 +0800

    [ALPS08758213] camsys: isp8: fix camsv bw
    [Description]
    correct camsv hrt bw
    
    [Test]
    Build pass and self-sanity pass
    
    MTK-Commit-Id: fc6217a384523b88aa225cb918d122db9d8a4345
    
    CR-Id: ALPS08758213
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I867e4ce86e735be2285be1fb5a1c2b8548674a8d
    Signed-off-by: Roy Yang <roy.yang@mediatek.com>

--- camsys/isp8/cam/mtk_cam-dvfs_qos.c
+++ camsys/isp8/cam/mtk_cam-dvfs_qos.c
@@ -1220,7 +1220,7 @@ static void apply_sv_qos(struct mtk_cam_job *job)
 
 		if (apply_bwr) {
 			int a_bw_w_KB = KBps_to_bwr(sv_avg_bw_w);
-			int p_bw_w_KB = KBps_to_bwr(sv_avg_bw_w);
+			int p_bw_w_KB = KBps_to_bwr(sv_peak_bw_w);
 
 			sv_avg_diff_bw_w = a_bw_w_KB - sv_dev->sv_avg_applied_bw_w;
 			sv_peak_diff_bw_w = p_bw_w_KB - sv_dev->sv_peak_applied_bw_w;

95d636489f503133b42e8bd19c5098e040a58b09
Yuming Wei <yu-ming.wei@mediatek.com>
2024-03-23 16:59:17 +0800

    [ALPS08809148] camsys: subsample state machine transit
    [Description]
    modify subsample apply/outer to processing
    from equal to greater
    
    [Test]
    pdas and ut
    
    MTK-Commit-Id: 1d1aac1fcf0ede31c38f1be6d26726a0fe7f5649
    
    CR-Id: ALPS08809148
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Yuming Wei <yu-ming.wei@mediatek.com>
    Change-Id: I53b90340c916b71c82a79adc6670f8420463dae7

--- camsys/isp8/cam/mtk_cam-job_state_subsample.c
+++ camsys/isp8/cam/mtk_cam-job_state_subsample.c
@@ -47,14 +47,14 @@ static struct state_transition STATE_TRANS(subsample, S_ISP_APPLYING)[] = {
 	},
 	{
 		S_ISP_PROCESSING, CAMSYS_EVENT_IRQ_L_SOF,
-		guard_inner_eq, 0
+		guard_inner_ge, 0
 	},
 };
 
 static struct state_transition STATE_TRANS(subsample, S_ISP_OUTER)[] = {
 	{
 		S_ISP_PROCESSING, CAMSYS_EVENT_IRQ_L_SOF,
-		guard_inner_eq, 0
+		guard_inner_ge, 0
 	},
 };
 

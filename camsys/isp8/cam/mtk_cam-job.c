4a94de73a469e1525127a8e7b98da1054dfefba5
Wen-jie Pan <Wen-jie.Pan@mediatek.com>
2024-04-01 20:30:46 +0800

    [ALPS08811152] camsv: fixed salmgr issue
    [Description]
    1. fixed apply sensor setting limited in sentest
    2. fixed KE in single sv case
    
    [Test]
    PDAS
    
    MTK-Commit-Id: a3b7f70927dcab0bcd445bccda5975db3b01fa20
    
    CR-Id: ALPS08811152
    Signed-off-by: Wen-jie Pan <Wen-jie.Pan@mediatek.com>
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: Iac8f2837f09e3ce5b11cb1ad9722f0f9150a0830

--- camsys/isp8/cam/mtk_cam-job.c
+++ camsys/isp8/cam/mtk_cam-job.c
@@ -4132,8 +4132,8 @@ static void update_job_state_init_sensor_param(struct mtk_cam_job *job)
 		infer_cq_trigger_deadline_ns(job, ctrl->frame_interval_ns);
 
 	job->job_state.s_params.always_allow =
-		ctrl_data->resource.user_data.raw_res.sen_apply_ctrl ==
-		MTK_CAM_SEN_APPLY_DIRECT_APPLY;
+		(ctrl_data && ctrl_data->resource.user_data.raw_res.sen_apply_ctrl ==
+		MTK_CAM_SEN_APPLY_DIRECT_APPLY) ? 1 : 0;
 
 	if (CAM_DEBUG_ENABLED(JOB))
 		pr_info("%s: job i2c_thres_ns %llu, latched_timing:%d, cq_trigger_thres:%llu always:%d\n",

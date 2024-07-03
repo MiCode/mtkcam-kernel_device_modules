bc5c9adcf20589d3e3d2e6ddf7b02aae7b3744ba
Auto Li <auto.li@mediatek.com>
2024-07-03 14:47:07 +0800

    [ALPS09088277] cam: Fix AOV wrong outmux tag
    [Description]
    Make sure UISP outmux config with tag 0.
    
    [Test]
    AOV test pass.
    
    MTK-Commit-Id: a10f9b9d9059ee29fa040739d38927390cda01e6
    
    Signed-off-by: Auto Li <auto.li@mediatek.com>
    CR-Id: ALPS09088277
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I2eb6e3c617d36984306223b0a695229d868a28ec

--- camsys/isp8/cam/mtk_cam-seninf-route.c
+++ camsys/isp8/cam/mtk_cam-seninf-route.c
@@ -2510,6 +2510,8 @@ int mtk_cam_seninf_s_aov_param(unsigned int sensor_id,
 		//g_aov_param.vc.dest[0].cam = 44;
 		g_aov_param.vc.dest[0].outmux = 13;
 		g_aov_param.vc.dest[0].pix_mode = 3;
+		g_aov_param.vc.dest[0].tag = 0;
+		g_aov_param.vc.dest[0].cam_type = TYPE_UISP;
 		g_aov_param.camtg = 13;
 	}
 

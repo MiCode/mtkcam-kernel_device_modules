b2d3d662c6101e3dfe675811df80ca7667cab255
niyongqi <niyongqi@xiaomi.com>
2026-07-04 15:31:08 +0800

    Kernel: Xiaomi mtkcam_kernel_device_module changes for Redmi Pad SE 4G / Redmi Pad SE 8.7 4G Android W
    The Patch based on MTK release TAG: t-alps-release-v0.mp1.tc8sp3-V1.121
    
    Signed-off-by: niyongqi <niyongqi@xiaomi.com>

---
+++ imgsensor/src-isp8/adaptor-hw.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2019 MediaTek Inc. */
+
+#ifndef __ADAPTOR_HW_H__
+#define __ADAPTOR_HW_H__
+
+int adaptor_hw_power_on(struct adaptor_ctx *ctx);
+int adaptor_hw_power_off(struct adaptor_ctx *ctx);
+int adaptor_hw_init(struct adaptor_ctx *ctx);
+int adaptor_hw_sensor_reset(struct adaptor_ctx *ctx);
+int adaptor_cam_pmic_on(struct adaptor_ctx *ctx);
+int adaptor_cam_pmic_off(struct adaptor_ctx *ctx);
+int adaptor_hw_deinit(struct adaptor_ctx *ctx);
+
+
+#endif
